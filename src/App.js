ac3a07e3d41983ee43c6979994ed976bfba0f6a7
vianarafael <vianarafaelds@gmail.com>
2021-03-01 12:37:14 +0900

    Can select and move the pawn

--- src/App.js
+++ src/App.js
@@ -30,18 +30,71 @@ function App() {
     "-3": <span>&#9821;</span>,
    "-4": <span>&#9820;</span>,
    "-5": <span>&#9819;</span>,
-   "-6": <span>&#9818;</span>
+   "-6": <span>&#9818;</span>,
+   "?": <span className="selectable" onClick={(e) => {
+     const [x, y] = e.target.parentNode.id.split("-");
+    //  selectOptions(x,y)
+    updateBoard(x,y)
+   }}>&#8855;</span>
+  }
+
+  function updateBoard(x, y) {
+    setBoard(board => {
+      const result = [...board];
+      board[x][y] = 1;
+      return result;
+    })
+  }
+
+  function selectOptions(x, y, [walkOne, walkTwo, killLeft, killRight]) {
+
+      setBoard(board => {
+      const result = [...board];
+      result[x][y] = 0;
+      if (walkOne) result[x-1][y] = "?";
+      if (walkTwo) result[x-2][y] = "?";
+      if (killLeft) result[x-1][y-1] = "?";
+      if (killRight) result[x-1][y+1] = "?";
+      return result;
+    })
+  }
+  function pawnWOptions(x,y) {
+      // 1. Choose location
+      let walkOne = false;
+      let walkTwo = false
+      let killLeft = false;
+      let killRight = false;
+    // 1.1 If front is free (x-1 = 0)- can select x-1
+    if (board[x-1][y] === 0) {
+      // make it blue and clickable 
+      walkOne = true;
+    }
+    // 1.2 If first move - can walk 2
+
+    if (Number(x) === 6) {
+      walkTwo = true;
+    }
+    
+    // 1.3 If enemy on the diagonal can kill it
+    if(board[x-1][y+1] < 0) {
+      killRight = true
+    }
+
+    if(board[x-1][y-1] < 0) {
+      killLeft = true
+    }
+    console.log(x,y,walkOne, walkTwo, killLeft, killRight)
+    selectOptions(x, y, [walkOne, walkTwo, killLeft, killRight])
   }
 
   function movePawnW(e) {
-    console.log(e.target.parentNode.id)
+
     const [x, y] = e.target.parentNode.id.split("-");
-    setBoard(board => {
-      const result = [...board];
-      result[x][y] = 0;
-      result[x-1][y] = 1;
-      return result;
-    })
+    pawnWOptions(x,y);
+
+
+
+    // 2. Update the board
   }
 
 
