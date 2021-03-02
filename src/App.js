2ca6eebae66db2b3e923857f2d3fe66cb71acec2
vianarafael <vianarafaelds@gmail.com>
2021-03-02 12:00:30 +0900

    The black pieces kind of work

--- src/App.js
+++ src/App.js
@@ -19,13 +19,13 @@ function App() {
   ];
   const [board, setBoard] = useState(initialBoard);
   const pieces = {
-    1: <PawnW onClick={movePawnW}>&#9817;</PawnW>,
+    1: <PawnW id="white" onClick={(e) =>  movePawn(e, "white")}>&#9817;</PawnW>,
     2: <span>&#9816;</span>,
     3: <span>&#9815;</span>,
     4: <span>&#9814;</span>,
     5: <span>&#9813;</span>,
     6: <span>&#9812;</span>,
-    "-1": <span>&#9823;</span>,
+    "-1": <span id="black"  onClick={(e) =>  movePawn(e, "black")}>&#9823;</span>,
     "-2": <span>&#9822;</span>,
     "-3": <span>&#9821;</span>,
    "-4": <span>&#9820;</span>,
@@ -33,12 +33,16 @@ function App() {
    "-6": <span>&#9818;</span>,
    "?": <span className="selectable" onClick={(e) => {
      const [x, y] = e.target.parentNode.id.split("-");
-    //  selectOptions(x,y)
-    updateBoard(x,y)
-   }}>&#8855;</span>
+    updateBoardW(x,y)
+   }}>&#8855;</span>,
+   "!": <span className="selectable" onClick={(e) => {
+    const [x, y] = e.target.parentNode.id.split("-");
+   updateBoardB(x,y)
+  }}>&#8855;</span>
   }
 
-  function updateBoard(x, y) {
+  function updateBoardW(x, y) {
+
     setBoard(board => {
       const result = [...board]
       board[x][y] = 1;
@@ -47,56 +51,117 @@ function App() {
     })
   }
 
-  function selectOptions(x, y, [walkOne, walkTwo, killLeft, killRight]) {
-    console.log(killLeft, killRight)
-      setBoard(board => {
-      const result = [...board];
-      result[x][y] = 0;
-      if (walkOne) result[x-1][y] = "?";
-      if (walkTwo) result[x-2][y] = "?";
-      if (killLeft) result[x-1][y-1] = "?";
-      if (killRight) result[x-1][Number(y)+1] = "?";
+  function updateBoardB(x, y) {
+
+    setBoard(board => {
+      const result = [...board]
+      board[x][y] = -1;
+      result.map((row, rowIndex) => row.map((square, squareIndex) => square === "!" ? board[rowIndex][squareIndex] = 0 : square))
       return result;
     })
   }
-  function pawnWOptions(x,y) {
+
+  function selectOptions(x, y, [walkOne, walkTwo, killLeft, killRight], color) {
+    console.log(color)
+      if (color === "white") {
+      setBoard(board => {
+        const result = [...board];
+        result[x][y] = 0;
+        if (walkOne) result[x-1][y] = "?";
+        if (walkTwo) result[x-2][y] = "?";
+        if (killLeft) result[x-1][y-1] = "?";
+        if (killRight) result[x-1][Number(y)+1] = "?";
+        return result;
+    })
+    } else {
+      setBoard(board => {
+        const result = [...board];
+        result[x][y] = 0;
+        if (walkOne) result[x+1][y] = "!";
+        if (walkTwo) result[x+2][y] = "!";
+        if (killLeft) result[x+1][Number(y)+1] = "!";
+        if (killRight) result[x+1][Number(y)-1] = "!";
+        return result;
+    })
+    }
+  }
+
+
+  function pawnOptions(x,y, color) {
+      x = Number(x);
+      y = Number(y)
       // 1. Choose location
       let walkOne = false;
       let walkTwo = false
       let killLeft = false;
       let killRight = false;
-    // 1.1 If front is free (x-1 = 0)- can select x-1
-    if (board[x-1][y] === 0) {
-      // make it blue and clickable 
-      walkOne = true;
-    }
-    // 1.2 If first move - can walk 2
 
-    if (Number(x) === 6) {
-      walkTwo = true;
-    }
+      if (color === "white") {
+        if (x === 0) {
+          // figure out that logic latter
+          console.log('change pieces')
+        }
     
-    // 1.3 If enemy on the diagonal can kill it
-    console.log('here',board[x-1][Number(y)+1], Number(y)+1)
-    if(board[x-1][Number(y)+1] < 0) {
-      killRight = true
+        // 1.1 If front is free (x-1 = 0)- can select x-1
+        if (board[x-1][y] === 0) {
+          // make it blue and clickable 
+          walkOne = true;
+        }
+        // 1.2 If first move - can walk 2
+    
+        if (Number(x) === 6) {
+          walkTwo = true;
+        }
+        
+        // 1.3 If enemy on the diagonal can kill it
+        if(board[x-1][Number(y)+1] < 0) {
+          killRight = true
+        }
+    
+        if(board[x-1][y-1] < 0) {
+          killLeft = true
+        }
+      }  else {
+        console.log("I am black")
+      if (x === 7) {
+          // figure out that logic latter
+          console.log('change pieces')
+        }
+    
+        // 1.1 If front is free (x-1 = 0)- can select x-1
+        if (board[x+1][y] === 0) {
+          // make it blue and clickable 
+          walkOne = true;
+        }
+        // 1.2 If first move - can walk 2
+    
+        if (x === 1) {
+          walkTwo = true;
+        }
+        
+        // 1.3 If enemy on the diagonal can kill it
+    
+        if (board[x+1][y-1] >= 0) {
+          killRight = true
+        }
+    
+        if (board[x+1][y+1] >= 0) {
+          killLeft = true
+      }
     }
 
-    if(board[x-1][y-1] < 0) {
-      killLeft = true
-    }
-    selectOptions(x, y, [walkOne, walkTwo, killLeft, killRight])
+    selectOptions(x, y, [walkOne, walkTwo, killLeft, killRight], color)
+      
   }
 
-  function movePawnW(e) {
 
+  function movePawn(e, color) {
+   
     const [x, y] = e.target.parentNode.id.split("-");
-    pawnWOptions(x,y);
+    pawnOptions(x,y,color);
 
-
-
-    // 2. Update the board
   }
+
 
 
   function displayBoard() {
