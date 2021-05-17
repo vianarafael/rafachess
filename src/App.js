fdc00d01392bac05b15ce54b516ab7fe68fa480b
something else <vianarafaelds@gmail.com>
2021-05-17 14:29:56 +0900

    something else

--- src/App.js
+++ src/App.js
@@ -1,219 +1,201 @@
 import "./App.css";
 import { useState, createContext, useReducer, useEffect } from "react";
 
-import Clock from './clock'
+import Clock from "./clock";
+// something else
+import selectMove from "./piecesLogic/selectMove";
+import Pawn from "./piecesLogic/pawn";
+import Knight from "./piecesLogic/knight";
+import Bishop from "./piecesLogic/bishop";
+import Rook from "./piecesLogic/rook";
+import Queen from "./piecesLogic/queen";
+import King from "./piecesLogic/king";
 
-import selectMove from './piecesLogic/selectMove'
-import Pawn from './piecesLogic/pawn'
-import Knight from "./piecesLogic/knight"
-import Bishop from "./piecesLogic/bishop"
-import Rook from "./piecesLogic/rook"
-import Queen from "./piecesLogic/queen"
-import King from "./piecesLogic/king"
-
-import socketIOClient from "socket.io-client"
+import socketIOClient from "socket.io-client";
 
 let socket;
 
-    // socket = socketIOClient("https://rafachess.xyz");
+// socket = socketIOClient("https://rafachess.xyz");
 
-   socket = socketIOClient("http://localhost:1234/");
+socket = socketIOClient("http://localhost:1234/");
 
-export const BoardContext = createContext()
-  const resetBoard = [
-    [-4, -2, -3, -5, -6, -3, -2, -4],
-    [-1, -1, -1, -1, -1, -1, -1, -1],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 1, 1, 1, 1, 1],
-    [4, 2, 3, 5, 6, 3, 2, 4],
-  ];
-  const initialBoard = [
-    [-4, -2, -3, -5, -6, -3, -2, -4],
-    [-1, -1, -1, -1, -1, -1, -1, -1],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 1, 1, 1, 1, 1],
-    [4, 2, 3, 5, 6, 3, 2, 4],
-  ];
-  //erase this
-let prev
-let selectedPiece
-  let setBoard = () => console.log('set board')
-const reducer = (state, action) =>
-{
-  if (action)
-  {
-    switch (action.type)
-    {
+export const BoardContext = createContext();
+const resetBoard = [
+  [-4, -2, -3, -5, -6, -3, -2, -4],
+  [-1, -1, -1, -1, -1, -1, -1, -1],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [4, 2, 3, 5, 6, 3, 2, 4],
+];
+const initialBoard = [
+  [-4, -2, -3, -5, -6, -3, -2, -4],
+  [-1, -1, -1, -1, -1, -1, -1, -1],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [4, 2, 3, 5, 6, 3, 2, 4],
+];
+//erase this
+let prev;
+let selectedPiece;
+let setBoard = () => console.log("set board");
+const reducer = (state, action) => {
+  if (action) {
+    switch (action.type) {
       case "setOptions":
         return action.payload;
       case "setPreviousBoard":
-        prev = action.payload
+        prev = action.payload;
       case "setSelectedPiece":
-        selectedPiece = action.payload
+        selectedPiece = action.payload;
     }
   }
-}
+};
 
-function App()
-  {
-  const [board, dispatch] = useReducer(reducer, initialBoard)
+function App() {
+  const [board, dispatch] = useReducer(reducer, initialBoard);
   const [turn, setTurn] = useState("white");
   const [finishedMove, setFinishedMove] = useState(false);
 
-  const [gameStarted, setGameStart] = useState(false)
-  const [playerColor, setPlayerColor] = useState("black")
+  const [gameStarted, setGameStart] = useState(false);
+  const [playerColor, setPlayerColor] = useState("black");
   const [gameRoom, setGameRoom] = useState();
-  
-  useEffect(() =>
-  {
-	  socket.on("move", (data) => {
-          console.log(data);
-          setTurn(data.turn);
-          dispatch({ type: "setOptions", payload: data.board });
-        });
-        socket.emit("move", {
-          board,
-          turn,
-          gameRoom
-        });
-    
-    socket.on("stats", ({ numClients, gameRoom }) =>
-    {
-      setGameRoom(gameRoom)
+
+  useEffect(() => {
+    socket.on("move", (data) => {
+      console.log(data);
+      setTurn(data.turn);
+      dispatch({ type: "setOptions", payload: data.board });
+    });
+    socket.emit("move", {
+      board,
+      turn,
+      gameRoom,
+    });
+
+    socket.on("stats", ({ numClients, gameRoom }) => {
+      setGameRoom(gameRoom);
       console.log("here", numClients);
       if (numClients === 1) setPlayerColor("white");
       else {
         setGameStart(true);
       }
     });
+  }, [finishedMove]);
 
-  }, [finishedMove])
-  
-
-
-    const pieces = {
-      1: <Pawn color="white" />,
-      2: <Knight color="white" />,
-      3: <Bishop color="white" />,
-      4: <Rook color="white" />,
-      5: <Queen color="white" />,
-      6: <King color="white" />,
-      "-1": <Pawn color="black" />,
-      "-2": <Knight color="black" />,
-      "-3": <Bishop color="black" />,
-      "-4": <Rook color="black" />,
-      "-5": <Queen color="black" />,
-      "-6": <King color="black" />,
-      "?": (
-        <span
-          className="selectable"
-          onClick={(e) =>
-            selectMove(
-              e,
-              prev,
-              selectedPiece,
-              (setBoard = dispatch),
-              setTurn,
-              finishedMove,
-              setFinishedMove
-            )
-          }
-        >
-          &#8855;
-        </span>
-      ),
-      "!": (
-        <span
-          className="selectable"
-          onClick={(e) => {
-            selectMove(
-              e,
-              prev,
-              selectedPiece,
-              (setBoard = dispatch),
-              setTurn,
-              finishedMove,
-              setFinishedMove
-            );
-          }}
-        >
-          &#8855;
-        </span>
-      ),
-    };
-
-
-    function displayBoard()
-    {
-      const check = {};
-
-      if (Array.isArray(board)) {
-        const result = board.map((row, indexRow) => {
-          let firstColor, secondColor;
-          if (indexRow % 2 === 0) {
-            firstColor = "white";
-            secondColor = "black";
-          } else {
-            firstColor = "black";
-            secondColor = "white";
-          }
-          return row.map((square, indexSquare) => {
-            check[square] = square;
-            return (
-              <div
-                id={`${indexRow}-${indexSquare}`}
-                className={indexSquare % 2 === 0 ? firstColor : secondColor}
-              >
-                {pieces[square]}
-              </div>
-            );
-          });
-        });
-        // if there are no ? && !
-        // && if there are no kings 6 && -6 -> end game
-        if (!check["?"] && !check["!"])
-        {
-          if (!check[6] || !check["-6"])
-          {
-            //dry - change this
-            return resetBoard.map((row, indexRow) => {
-              let firstColor, secondColor;
-              if (indexRow % 2 === 0) {
-                firstColor = "white";
-                secondColor = "black";
-              } else {
-                firstColor = "black";
-                secondColor = "white";
-              }
-              return row.map((square, indexSquare) => {
-                check[square] = square;
-                return (
-                  <div
-                    id={`${indexRow}-${indexSquare}`}
-                    className={indexSquare % 2 === 0 ? firstColor : secondColor}
-                  >
-                    {pieces[square]}
-                  </div>
-                );
-              });
-            });
-          }
+  const pieces = {
+    1: <Pawn color="white" />,
+    2: <Knight color="white" />,
+    3: <Bishop color="white" />,
+    4: <Rook color="white" />,
+    5: <Queen color="white" />,
+    6: <King color="white" />,
+    "-1": <Pawn color="black" />,
+    "-2": <Knight color="black" />,
+    "-3": <Bishop color="black" />,
+    "-4": <Rook color="black" />,
+    "-5": <Queen color="black" />,
+    "-6": <King color="black" />,
+    "?": (
+      <span
+        className="selectable"
+        onClick={(e) =>
+          selectMove(
+            e,
+            prev,
+            selectedPiece,
+            (setBoard = dispatch),
+            setTurn,
+            finishedMove,
+            setFinishedMove
+          )
         }
-        return result;
+      >
+        &#8855;
+      </span>
+    ),
+    "!": (
+      <span
+        className="selectable"
+        onClick={(e) => {
+          selectMove(
+            e,
+            prev,
+            selectedPiece,
+            (setBoard = dispatch),
+            setTurn,
+            finishedMove,
+            setFinishedMove
+          );
+        }}
+      >
+        &#8855;
+      </span>
+    ),
+  };
+
+  function displayBoard() {
+    const check = {};
+
+    if (Array.isArray(board)) {
+      const result = board.map((row, indexRow) => {
+        let firstColor, secondColor;
+        if (indexRow % 2 === 0) {
+          firstColor = "white";
+          secondColor = "black";
+        } else {
+          firstColor = "black";
+          secondColor = "white";
+        }
+        return row.map((square, indexSquare) => {
+          check[square] = square;
+          return (
+            <div
+              id={`${indexRow}-${indexSquare}`}
+              className={indexSquare % 2 === 0 ? firstColor : secondColor}
+            >
+              {pieces[square]}
+            </div>
+          );
+        });
+      });
+      // if there are no ? && !
+      // && if there are no kings 6 && -6 -> end game
+      if (!check["?"] && !check["!"]) {
+        if (!check[6] || !check["-6"]) {
+          //dry - change this
+          return resetBoard.map((row, indexRow) => {
+            let firstColor, secondColor;
+            if (indexRow % 2 === 0) {
+              firstColor = "white";
+              secondColor = "black";
+            } else {
+              firstColor = "black";
+              secondColor = "white";
+            }
+            return row.map((square, indexSquare) => {
+              check[square] = square;
+              return (
+                <div
+                  id={`${indexRow}-${indexSquare}`}
+                  className={indexSquare % 2 === 0 ? firstColor : secondColor}
+                >
+                  {pieces[square]}
+                </div>
+              );
+            });
+          });
+        }
       }
+      return result;
     }
-  
-  
+  }
 
-  
-
-  
   return (
     <>
       {gameStarted ? (
@@ -232,6 +214,6 @@ function App()
       )}
     </>
   );
-  }
+}
 
 export default App;
